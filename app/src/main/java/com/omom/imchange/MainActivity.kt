972a9f71a89d241152f2e9faab54855c673c252e
Om0m <em49amihso@gmail.com>
2020-03-06 11:13:36 +0900

    delete: Unnecessary code

--- app/src/main/java/com/omom/imchange/MainActivity.kt
+++ app/src/main/java/com/omom/imchange/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,6 +21,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val bitmap = BitmapFactory.decodeStream(assets.open("akarenga.jpg"))
+        imageViewInput.setImageBitmap(scaleChangeBitmap(bitmap, 2.0f))
+        imageViewOutput.setImageBitmap(scaleChangeBitmap(bitmap, 2.0f))
+
 
         val module = Module.load(assetFilePath(this, "mosaic.pt"))
 
@@ -29,27 +31,17 @@ class MainActivity : AppCompatActivity() {
                 bitmap,
                 TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB
         )
-
         val outputTensor = module.forward(IValue.from(inputTensor)).toTensor()
-        Log.d("MainActivity", outputTensor.toString())
         val outputFloatArray = outputTensor.dataAsFloatArray
-        Log.d("MainActivity", outputFloatArray.size.toString())
         val outputByteArray = outputFloatArray.foldIndexed(ByteArray(outputFloatArray.size)) { i, a, v -> a.apply { set(i, v.toByte()) }}
-        Log.d("MainActivity", outputByteArray.size.toString())
 
-//        val opt = BitmapFactory.Options()
-//        opt.inJustDecodeBounds = false
-//        val outputBitmap = BitmapFactory.decodeByteArray(outputByteArray, 0, outputByteArray.size, opt)
-
-        val bmp = Bitmap.createBitmap(640, 426, Bitmap.Config.ALPHA_8)
-//        val bmp2 = Bitmap.createBitmap(640, 428, 3, Bitmap.Config.ALPHA_8)
+        val bmp = Bitmap.createBitmap(640, 428, Bitmap.Config.ALPHA_8)
         val buffer: ByteBuffer = ByteBuffer.wrap(outputByteArray)
         bmp.copyPixelsFromBuffer(buffer)
 
-        val button = findViewById<Button>(R.id.transButton)
+        val button = findViewById<Button>(R.id.mosaicButton)
         button.setOnClickListener {
-//            Log.d("MainActivity", outputBitmap.byteCount.toString())
-            imageViewOutput.setImageBitmap(bmp)
+            imageViewOutput.setImageBitmap(scaleChangeBitmap(bmp, 2.0f))
         }
     }
 }
@@ -70,4 +62,8 @@ private fun assetFilePath(context: Context, assetName: String): String {
         }
         return file.absolutePath
     }
+}
+
+private fun scaleChangeBitmap(bitmap: Bitmap, ratio: Float): Bitmap {
+    return Bitmap.createScaledBitmap(bitmap, (bitmap.width * ratio).toInt(), (bitmap.height * ratio).toInt(), true)
 }
